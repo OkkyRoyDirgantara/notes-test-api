e7142f93247a8328cacbc04e2570c4b998272091
OkkyRoyDirgantara <okkyroydirgantara@gmail.com>
2021-08-22 13:23:02 +0700

    Add validation on project

--- src/api/notes/index.js
+++ src/api/notes/index.js
@@ -4,8 +4,8 @@ const routes = require('./routes');
 module.exports = {
   name: 'notes',
   version: '1.0.0',
-  register: async (server, { service }) => {
-    const noteshandler = new NotesHandler(service);
+  register: async (server, { service, validator }) => {
+    const noteshandler = new NotesHandler(service, validator);
     server.route(routes(noteshandler));
   },
 };
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
 
 const init = async () => {
   const notesService = new NotesService();
@@ -16,11 +17,11 @@ const init = async () => {
 
   });
 
-
   await server.register({
     plugin: notes,
     options: {
       service: notesService,
+      validator: NotesValidator,
     },
   });
 
